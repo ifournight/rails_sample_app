2feafa0e4b7b1752098d67d45686db63a9af9dc2
ifournight <ifournight@gmail.com>
2017-02-20 15:41:31 +0800

    Hide and add a new asw credential for security

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -9,10 +9,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',       
-      aws_access_key_id:     'AKIAI22DA4WEUFJ6P6KA',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
+      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],
+      region: ENV['S3_REGION']
     }
-    config.fog_directory = 'railssampleappsh'
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
